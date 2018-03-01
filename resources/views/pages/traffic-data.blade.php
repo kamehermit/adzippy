abf70c5f1699e579abd02022e9390d4c1b6d81da
animeshkuzur <animesh.kuzur@outlook.com>
2018-03-02 00:56:32 +0530

    traffic data update

--- resources/views/pages/traffic-data.blade.php
+++ resources/views/pages/traffic-data.blade.php
@@ -9,6 +9,11 @@
        * element that contains the map. */
       #map {
         height: 100%;
+        z-index: 0;
+        position: absolute;
+        left: 0;
+        right:0;
+        top:0;
       }
       /* Optional: Makes the sample page fill the window. */
       html, body {
@@ -16,10 +21,62 @@
         margin: 0;
         padding: 0;
       }
+      .coordinates{
+        position: relative;
+        z-index: 100;
+        height: 100vh;
+        width: 100vw;
+        font-size: 11px;
+        font-weight: 500;
+      }
+      #tl{
+        
+        position: absolute;
+        top:0;
+        left: 50%;
+      }
+      #ll{
+        
+        position: absolute;
+        top:50%;
+        left: 0;
+/*        transform: rotate(90deg);*/
+        
+      }
+      #rl{
+
+        position: absolute;
+        top:50%;
+        right: 0;
+        /*transform: rotate(90deg);*/
+      }
+      #bl{
+
+        position: absolute;
+        bottom:0;
+        left: 50%;
+      }
+
     </style>
   </head>
   <body>
-    <div id="map"></div>
+    <div class="coordinates">
+      <div id="tl">
+        
+      </div>
+      <div id="ll">
+        
+      </div>
+      <div id="rl">
+        
+      </div>
+      <div id="bl">
+        
+      </div>
+    </div>
+    <div id="map">
+      
+    </div>
     <script>
       function initMap() {
         var customMapType = new google.maps.StyledMapType([ { "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative", "elementType": "geometry", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative.land_parcel", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative.neighborhood", "stylers": [ { "visibility": "off" } ] }, { "featureType": "landscape", "stylers": [ { "visibility": "off" } ] }, { "featureType": "landscape.natural.terrain", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road", "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "stylers": [ { "visibility": "off" } ] } ], {
@@ -63,15 +120,26 @@
         trafficLayer.setMap(map);
 
 
-        var e_log = document.createElement('p');
+        /*var e_log = document.createElement('p');
         e_log.style.color = '#2d2d2d';
-        e_log.innerHTML = "-"+String(bounds.b.b).substring(0,9)+"-";
-        var E_log = document.createElement('div');
+        e_log.innerHTML = "-"+String(bounds.b.b).substring(0,9)+"-";*/
+        var E_log = document.getElementById('ll');
+        E_log.innerHTML = String(bounds.b.b).substring(0,9);
+
+        var W_log = document.getElementById('rl');
+        W_log.innerHTML = String(bounds.b.f).substring(0,9);
+
+        var N_log = document.getElementById('tl');
+        N_log.innerHTML = String(bounds.f.f).substring(0,9);
+
+        var S_log = document.getElementById('bl');
+        S_log.innerHTML = String(bounds.f.b).substring(0,9);
+        /*var E_log = document.createElement('div');
           E_log.setAttribute("class", "E_log"); 
-          E_log.appendChild(e_log);
+          E_log.appendChild(e_log);*/
 
 
-          var w_log = document.createElement('p');
+          /*var w_log = document.createElement('p');
         w_log.style.color = '#2d2d2d';
         w_log.innerHTML = "-"+String(bounds.b.f).substring(0,9)+"-";
         var W_log = document.createElement('div');
@@ -88,13 +156,15 @@
         s_log.innerHTML = "-"+String(bounds.f.b).substring(0,9)+"-";
         var S_log = document.createElement('div');
           S_log.appendChild(s_log);
+*/
 
-
-        map.controls[google.maps.ControlPosition.LEFT_CENTER].push(E_log);
+        /*map.controls[google.maps.ControlPosition.LEFT_CENTER].push(E_log);
         
         map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(W_log);
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(N_log);
-        map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(S_log);
+        map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(S_log);*/
+
+
         /*if(document.getElementById("E_log").hasChildNodes() ){
           E_log.replaceChild(e_log,e_log);  
         }
